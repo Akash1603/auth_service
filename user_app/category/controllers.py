d348495f53f4dadecb825ffce05b105ad7ece975
Aakash Bawaskar <aakash.bawaskar@taskeasy.com>
2021-06-08 00:18:48 +0530

    Add category list API.

--- user_app/category/controllers.py
+++ user_app/category/controllers.py
@@ -1,13 +1,20 @@
 from rest_framework.response import Response
 from service_objects.services import Service
 
-from user_app.models import Category, UserProfile
+from user_app.category.serializers import createCategorySerializer
+from user_app.models import Category
 
 
 class CreateCategoryService(Service):
     def process(self):
         category = Category.objects.create(
             name=self.data.get("data").get("name"),
-            user_profile=UserProfile.objects.get(id=self.data.get("user").id),
+            user_profile=self.data.get("user")
         )
         return Response({"name": category.name}, 201)
+
+
+class ListCategoryService(Service):
+    def process(self):
+        categories = Category.objects.filter(user_profile=self.data.get("user"))
+        return Response(createCategorySerializer(categories, many=True).data, 200)
--- user_app/category/views.py
+++ user_app/category/views.py
@@ -4,7 +4,7 @@ from oauth2_provider.contrib.rest_framework import (
     OAuth2Authentication,
 )
 
-from user_app.category.controllers import CreateCategoryService
+from user_app.category.controllers import CreateCategoryService, ListCategoryService
 from user_app.category.permissions import OnlyFirstNameAkashPermission
 from user_app.category.serializers import createCategorySerializer
 
@@ -14,6 +14,7 @@ class CreateCategoryView(APIView):
     permission_classes = [TokenMatchesOASRequirements, OnlyFirstNameAkashPermission]
     required_alternate_scopes = {
         "POST": [["create"]],
+        "GET": [["read"]]
     }
 
     @staticmethod
@@ -23,3 +24,7 @@ class CreateCategoryView(APIView):
             return CreateCategoryService.execute(
                 {"data": dict(serial_data.validated_data), "user": request.user}
             )
+
+    @staticmethod
+    def get(request):
+        return ListCategoryService.execute({"user": request.user})
